f7bf0f00a33bfcac358f8c616f2305ac30820a5d
Fernanda <feer_nanda18@hotmail.com>
2023-10-30 21:53:19 -0300

    inclusão do campo estado civil

--- Agenda/Agenda/Scripts/activate.bat
+++ Agenda/Agenda/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\fernanda.adesanto\Source\Repos\AgendaVenv
+set VIRTUAL_ENV=C:\Users\fernanda.adesanto\source\repos\AgendaVenv\agenda\agenda
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -16,7 +16,7 @@ if defined _OLD_VIRTUAL_PROMPT set PROMPT=%_OLD_VIRTUAL_PROMPT%
 if defined _OLD_VIRTUAL_PYTHONHOME set PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%
 
 set _OLD_VIRTUAL_PROMPT=%PROMPT%
-set PROMPT=(AgendaVenv) %PROMPT%
+set PROMPT=(agenda) %PROMPT%
 
 if defined PYTHONHOME set _OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%
 set PYTHONHOME=
@@ -25,7 +25,7 @@ if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
 set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
-set VIRTUAL_ENV_PROMPT=(AgendaVenv) 
+set VIRTUAL_ENV_PROMPT=(agenda) 
 
 :END
 if defined _OLD_CODEPAGE (
--- Agenda/AgendaApp/models.py
+++ Agenda/AgendaApp/models.py
@@ -2,6 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Contato(models.Model):
+
+#Opções do campo Estado Civil
+#Primeiro valor da tupla é oq vai no banco, o segunda mostra na tela
+    ESTADO_CIVIS = [('S','Solteiro'), ('C','Casado'), ('D','Divorciado'), ('V','Viúvo')]
+
     nome = models.CharField(max_length=100)
     apelido = models.CharField(max_length=30)
     email = models.EmailField(max_length=100)
@@ -13,6 +18,7 @@ class Contato(models.Model):
     bairro = models.CharField(max_length=100)
     cidade = models.CharField(max_length=100)
     estado = models.CharField(max_length=50)
+    estado_civis = models.CharField(max_length=1, choices=ESTADO_CIVIS, null=True)
 
     def __str__(self):
         return self.nome
